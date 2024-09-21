9a6ad71c7277e2dad57f3c7b32bb944f0a79663a
Shayerine <lordacaelusmuintir@hotmail.com>
2024-09-21 16:30:12 -0500

    build Navbar with response to scrolling

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,32 +1,28 @@
-import useCurrentUser from "@/hooks/useCurrentUser";
+import Navbar from "@/components/Navbar";
 import { NextPageContext } from "next";
-import { getSession, signOut } from "next-auth/react";
+import { getSession } from "next-auth/react";
 
 export async function getServerSideProps(context: NextPageContext) {
   const session = await getSession(context);
 
-  if(!session){
+  if (!session) {
     return {
       redirect: {
-        destination: '/auth',
-        permanent: false
-      }
-    }
+        destination: "/auth",
+        permanent: false,
+      },
+    };
   }
 
   return {
-    props: {}
-  }
+    props: {},
+  };
 }
 
 export default function Home() {
-
-  const { data: user } = useCurrentUser();
-
-  return (<>
-    <h1 className="text-2xl text-green-500">Netflix Clone</h1>
-    <p className="text-white" >Logged in as {user?.email}</p>
-    <button className="h-10 w-full bg-white" onClick={() => signOut()}>Sign Out</button>
+  return (
+    <>
+      <Navbar />
     </>
   );
 }
